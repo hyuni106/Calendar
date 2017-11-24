9e8dd6409105cb3a23e02c632656d5448f8a4d59
hyuni106 <30651511+hyuni106@users.noreply.github.com>
2017-11-24 18:21:01 +0900

    commit

--- app/src/main/java/kr/co/tjeit/calendar/fragment/CalendarFragment.java
+++ app/src/main/java/kr/co/tjeit/calendar/fragment/CalendarFragment.java
@@ -1,11 +1,26 @@
 package kr.co.tjeit.calendar.fragment;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.format.DateFormatTitleFormatter;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import kr.co.tjeit.calendar.R;
 
@@ -14,10 +29,13 @@ import kr.co.tjeit.calendar.R;
  */
 
 public class CalendarFragment extends Fragment {
+    private com.prolificinteractive.materialcalendarview.MaterialCalendarView calendarView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_calendar, container, false);
+        this.calendarView = (MaterialCalendarView) v.findViewById(R.id.calendarView);
         return v;
     }
 
@@ -29,7 +47,61 @@ public class CalendarFragment extends Fragment {
     }
 
     private void setValues() {
+        settingCalendar();
+    }
 
+    private void settingCalendar() {
+        SimpleDateFormat myDateFormat = new SimpleDateFormat("yyyy년 MM월");
+        DateFormatTitleFormatter formatter = new DateFormatTitleFormatter(myDateFormat);
+        calendarView.setTitleFormatter(formatter);
+        calendarView.addDecorator(new DayViewDecorator() {
+            @Override
+            public boolean shouldDecorate(CalendarDay day) {
+                // check if weekday is sunday
+                return day.getCalendar().get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY;
+            }
+
+            @Override
+            public void decorate(DayViewFacade view) {
+                // add red foreground span
+                view.addSpan(new ForegroundColorSpan(
+                        ContextCompat.getColor(getContext(), R.color.red)));
+            }
+        });
+
+        calendarView.addDecorator(new DayViewDecorator() {
+            @Override
+            public boolean shouldDecorate(CalendarDay day) {
+                // check if weekday is sunday
+                return day.getCalendar().get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY;
+            }
+
+            @Override
+            public void decorate(DayViewFacade view) {
+                // add red foreground span
+                view.addSpan(new ForegroundColorSpan(
+                        ContextCompat.getColor(getContext(), R.color.blue)));
+            }
+        });
+
+        final CalendarDay date = CalendarDay.today();
+
+        calendarView.setSelectedDate(date);
+        calendarView.addDecorator(new DayViewDecorator() {
+
+            @Override
+            public boolean shouldDecorate(CalendarDay day) {
+                return date != null && day.equals(date);
+            }
+
+            @Override
+            public void decorate(DayViewFacade view) {
+                view.addSpan(new StyleSpan(Typeface.BOLD));
+                view.addSpan(new RelativeSizeSpan(1.2f));
+                view.addSpan(new ForegroundColorSpan(
+                        ContextCompat.getColor(getContext(), R.color.firstColor)));
+            }
+        });
     }
 
     private void setupEvents() {
